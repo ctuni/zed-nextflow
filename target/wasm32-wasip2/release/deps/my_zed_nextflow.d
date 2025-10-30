0d88a4ac29f210df361486142b22b9b2ca885d05
DLBPointon <damonlbp@hotmail.co.uk>
2025-10-30 13:43:49 +0000

    Some additions to the highlights

--- target/wasm32-wasip2/release/deps/my_zed_nextflow.d
+++ target/wasm32-wasip2/release/deps/my_zed_nextflow.d
@@ -1,5 +1,5 @@
-/home/ctuni/zed-nextflow/target/wasm32-wasip2/release/deps/my_zed_nextflow.d: src/lib.rs
+/Users/dp24/Library/Application Support/Zed/extensions/installed/nextflow/target/wasm32-wasip2/release/deps/my_zed_nextflow.wasm: src/lib.rs
 
-/home/ctuni/zed-nextflow/target/wasm32-wasip2/release/deps/my_zed_nextflow.wasm: src/lib.rs
+/Users/dp24/Library/Application Support/Zed/extensions/installed/nextflow/target/wasm32-wasip2/release/deps/my_zed_nextflow.d: src/lib.rs
 
 src/lib.rs:
--- target/wasm32-wasip2/release/my_zed_nextflow.d
+++ target/wasm32-wasip2/release/my_zed_nextflow.d
@@ -1,1 +1,1 @@
-/home/ctuni/zed-nextflow/target/wasm32-wasip2/release/my_zed_nextflow.wasm: /home/ctuni/zed-nextflow/src/lib.rs
+/Users/dp24/Library/Application\ Support/Zed/extensions/installed/nextflow/target/wasm32-wasip2/release/my_zed_nextflow.wasm: /Users/dp24/Documents/zed-nextflow/zed-nextflow/src/lib.rs
